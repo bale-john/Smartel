5f8f4f94500a1431354dae81a1004fd7f0b443fa
bale-john <1401214269@pku.edu.cn>
2015-12-05 15:13:54 +0800

    add different types of rooms

--- test.cpp
+++ test.cpp
@@ -10,6 +10,37 @@
 #include"./csv_parser/csv_writer.h"
 using namespace std;
 
+//get history data
+void getHistoryData(vector<pair<int, int>> &priceQuantity, map<int, int> &pr){
+	ifstream icin;
+	icin.open("../files/HOTEL_SALES.csv");
+	vector<string> result;
+	CSVReader myreader(icin);
+	string price = "";
+	string quantity = "";
+	int p = 0, q = 0, tr = 0;
+	myreader.read_next(result);
+	cout << "choose the type of room: 1.Single\t2.Double\t3.Triple" << endl;
+	int type = 0;
+	cin >> type;
+	vector<string> roomType = {"Single", "Double", "Triple"};
+	while (!result.empty()){
+		myreader.read_next(result);
+		if (result.size() >= 2 && roomType[type-1] != result[2]){
+			continue;
+		}
+		price = result[0];
+		p = atoi(&(price[0]));
+		quantity = result[1];
+		q = atoi(&(quantity[0]));
+		tr = p*q;
+		priceQuantity.push_back(make_pair(p, q));
+		pr[p] = tr;
+	}
+	icin.close();
+	return;
+}
+
 //computer the elasticity in the price elasticity of demand.
 double getElasticity(int currentPrice, map<int, int> &pr){
 	double elasticity = 0;
@@ -38,14 +69,26 @@ double getElasticity(int currentPrice, map<int, int> &pr){
 	return -deltaQuantity / deltaPrice;
 }
 
-//write to the result.csv
-void outputToFile(const vector<vector<string>> &output){
-	ofstream ocout;
-	ocout.open("../output/result.csv");
-	CSVWriter mywriter(ocout);
-	mywriter.write_all(output);
-	ocout.close();
-	return;
+//get the maxprice and maxrevenue
+pair<int, int> getMax(const vector<pair<int, int>> &priceQuantity, map<int, int> &pr){
+	//get the relationship between price and quantity
+	for (int i = 1; i < priceQuantity.size(); ++i){
+		double k = ((double)(priceQuantity[i].second - priceQuantity[i-1].second)) / (priceQuantity[i].first - priceQuantity[i-1].first);
+		double b = priceQuantity[i].second - (double)k * (priceQuantity[i].first);
+		for (int num = priceQuantity[i-1].first + 1; num < priceQuantity[i].first; ++num){
+			pr[num] = num * (k * num + b);
+		}
+	}
+	//get the maxprice and max revenue
+	int maxrevenue = 0;
+	int maxprice = 0;
+	for (map<int, int>::iterator it = pr.begin(); it != pr.end(); it++){
+		if (it->second > maxrevenue){
+			maxrevenue = it->second;
+			maxprice = it->first;
+		}
+	}
+	return make_pair(maxprice, maxrevenue);
 }
 
 //recommend the price and tell out the reason
@@ -82,52 +125,15 @@ void interactive(vector<vector<string>> &output, const vector<pair<int, int>> &p
 	return;
 }
 
-//get history data
-void getHistoryData(vector<pair<int, int>> &priceQuantity, map<int, int> &pr){
-	ifstream icin;
-	icin.open("../files/HOTEL_SALES.csv");
-	vector<string> result = {"start"};
-	CSVReader myreader(icin);
-	string price = "";
-	string quantity = "";
-	int p = 0, q = 0, tr = 0;
-	while (!result.empty()){
-		myreader.read_next(result);
-		price = result[0];
-		p = atoi(&(price[0]));
-		quantity = result[1];
-		q = atoi(&(quantity[0]));
-		tr = p*q;
-		priceQuantity.push_back(make_pair(p, q));
-		pr[p] = tr;
-	}
-	icin.close();
+//write to the result.csv
+void outputToFile(const vector<vector<string>> &output){
+	ofstream ocout;
+	ocout.open("../output/result.csv");
+	CSVWriter mywriter(ocout);
+	mywriter.write_all(output);
+	ocout.close();
 	return;
 }
-
-//get the maxprice and maxrevenue
-
-//get the relationship between price and quantity
-pair<int, int> getMax(const vector<pair<int, int>> &priceQuantity, map<int, int> &pr){
-	for (int i = 1; i < priceQuantity.size(); ++i){
-		double k = ((double)(priceQuantity[i].second - priceQuantity[i-1].second)) / (priceQuantity[i].first - priceQuantity[i-1].first);
-		double b = priceQuantity[i].second - (double)k * (priceQuantity[i].first);
-		for (int num = priceQuantity[i-1].first + 1; num < priceQuantity[i].first; ++num){
-			pr[num] = num * (k * num + b);
-		}
-	}
-	//get the maxprice and max revenue
-	int maxrevenue = 0;
-	int maxprice = 0;
-	for (map<int, int>::iterator it = pr.begin(); it != pr.end(); it++){
-		if (it->second > maxrevenue){
-			maxrevenue = it->second;
-			maxprice = it->first;
-		}
-	}
-	return make_pair(maxprice, maxrevenue);
-}
-
 
 int main(int argc, char** argv){
 	//store the price and quantity
